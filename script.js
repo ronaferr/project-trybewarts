1b4306c5da81da2510b4a2b488269daca28f7655
Gabriela Menezes <gahbi.menezes@gmail.com>
2022-02-25 16:35:53 -0300

    adicionado o botao Enviar no form de login

---
+++ script.js
@@ -0,0 +1,11 @@
+const email = document.querySelector('#email');
+const senha = document.querySelector('#senha')
+const btnLogin = document.querySelector('#btn-login');
+
+btnLogin.addEventListener('click', () => {
+    if (email.value === 'tryber@teste.com' && senha.value == '123456') {
+        alert('Olá, Tryber!');
+    } else {
+        alert('Email ou senha inválidos.');
+    }
+})
